2729506664e6088f24b5e9c76350ae13757a204c
MiYABiS <miyabis@live.jp>
2017-07-22 13:10:20 +0900

    ソリューション構成、プロジェクト構成の変更

---
+++ MocaWeb/My Project/AssemblyInfo.vb
@@ -0,0 +1,83 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyDescription("MocaWeb")>
+<Assembly: AssemblyCompany("MiYABiS")> 
+<Assembly: AssemblyProduct("MocaWeb")> 
+<Assembly: AssemblyCopyright("Copyright © 2011 MiYABiS All Rights Reserved.")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("334e9cee-0d47-4d70-924b-b5098a3432cb")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+' プログラム要素が CLS (Common Language Specification) に準拠しているかどうかを示します
+<Assembly: System.CLSCompliant(True)>
+
+
+#If net20 Then
+<Assembly: AssemblyVersion("2.0.1")>
+<Assembly: AssemblyFileVersion("2.0.1")>
+<Assembly: AssemblyTitle("Moca.NET Web .NET 2.0")>
+<Assembly: AssemblyInformationalVersion("2.0.1 .NET 2.0")>
+#End If
+#If net35 Then
+<Assembly: AssemblyVersion("3.5.1")>
+<Assembly: AssemblyFileVersion("3.5.1")>
+<Assembly: AssemblyTitle("Moca.NET Web .NET 3.5")>
+<Assembly: AssemblyInformationalVersion("3.5.1 .NET 3.5")>
+#End If
+#If net40 Then
+<Assembly: AssemblyVersion("4.0.1")>
+<Assembly: AssemblyFileVersion("4.0.1")>
+<Assembly: AssemblyTitle("Moca.NET Web .NET 4.0")>
+<Assembly: AssemblyInformationalVersion("4.0.1 .NET 4.0")>
+#End If
+#If net45 Then
+<Assembly: AssemblyVersion("4.5.0")>
+<Assembly: AssemblyFileVersion("4.5.0")>
+<Assembly: AssemblyTitle("Moca.NET Web .NET 4.5")>
+<Assembly: AssemblyInformationalVersion("4.5.0 .NET 4.5")>
+#End If
+#If net452 Then
+<Assembly: AssemblyVersion("4.5.2")>
+<Assembly: AssemblyFileVersion("4.5.2")>
+<Assembly: AssemblyTitle("Moca.NET Web .NET 4.5.2")>
+<Assembly: AssemblyInformationalVersion("4.5.2 .NET 4.5.2")>
+#End If
+#If net46 Then
+<Assembly: AssemblyVersion("4.6.0")>
+<Assembly: AssemblyFileVersion("4.6.0")>
+<Assembly: AssemblyTitle("Moca.NET Web .NET 4.6")>
+<Assembly: AssemblyInformationalVersion("4.6.0 .NET 4.6")>
+#End If
+#If net462 Then
+<Assembly: AssemblyVersion("4.6.2")>
+<Assembly: AssemblyFileVersion("4.6.2")>
+<Assembly: AssemblyTitle("Moca.NET Web .NET 4.6.2")>
+<Assembly: AssemblyInformationalVersion("4.6.2 .NET 4.6.2")>
+#End If
+#If net47 Then
+<Assembly: AssemblyVersion("4.7.0")>
+<Assembly: AssemblyFileVersion("4.7.0")>
+<Assembly: AssemblyTitle("Moca.NET Web .NET 4.7")>
+<Assembly: AssemblyInformationalVersion("4.7.0 .NET 4.7")>
+#End If
